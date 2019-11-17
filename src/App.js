7de587127596d7efef1dffa3a27c8902f6dedacd
silvestrevivo <silvestrevivomillan@gmail.com>
2019-11-17 22:52:17 +0100

    components, states, props and env variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount(){
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    if(res){
+      this.setState({users: res.data, loading: false});
+    }
+  }
   render(){
+    const { users, loading } = this.state
     return (
       <div className="App">
-        <h1>Learn React</h1>
+        <Navbar />
+        <div className="container">
+          <Users loading={loading} users={users}/>
+        </div>
       </div>
     );
   }
