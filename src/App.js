e02c7c39b147add9915eef2517cf29fa2f1f8687
silvestrevivo <silvestrevivomillan@gmail.com>
2019-11-18 23:43:56 +0100

    search bar added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
 
 class App extends Component {
@@ -9,20 +10,24 @@ class App extends Component {
     users: [],
     loading: false
   }
-  async componentDidMount(){
+
+  searchUser = async text => {
     this.setState({loading: true});
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
     if(res){
-      this.setState({users: res.data, loading: false});
+      this.setState({users: res.data.items, loading: false});
     }
   }
+
   render(){
     const { users, loading } = this.state
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users loading={loading} users={users}/>
+          <Search searchUser={this.searchUser} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
